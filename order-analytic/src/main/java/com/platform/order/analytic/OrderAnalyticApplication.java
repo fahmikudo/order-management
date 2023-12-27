b8dd2316c85ea1f4437bcd2b35d0b4d0585ad3a5
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2023-12-27 21:41:46 +0700

    initialize projects

---
+++ order-analytic/src/main/java/com/platform/order/analytic/OrderAnalyticApplication.java
@@ -0,0 +1,13 @@
+package com.platform.order.analytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAnalyticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAnalyticApplication.class, args);
+	}
+
+}
